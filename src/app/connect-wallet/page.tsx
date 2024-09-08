94da2f6261c9a74a2119f31eaf6a2e13acdf9ce9
KaushikKC <kccreations1704@gmail.com>
2024-09-08 18:43:57 +0530

    Moved the corekit and evmProvider in seperate file

--- src/app/connect-wallet/page.tsx
+++ src/app/connect-wallet/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+"use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Navbar from "../../components/Navbar";
